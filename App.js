163193d1f3f905a4a5ce28e5b547346608680395
Gabriel <pesoa01@gmail.com>
2023-09-21 07:31:02 -0300

    apk build ok

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google'
+import axios, { AxiosHeaders } from 'axios';
 
 //web 390958680596-vtonuian8j15vrjq0sr9k352ddulcs85.apps.googleusercontent.com
 //iOS 390958680596-t0gg07uga0sc89e9h5kdgocuir9ot1uv.apps.googleusercontent.com
@@ -13,7 +14,7 @@ WebBrowser.maybeCompleteAuthSession();
 export default function App() {
   const [accessToken, setAccessToken] = React.useState(null);
   const [user, setUser] = React.useState(null);
-  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+  const [request, response, promptAsync] = Google.useAuthRequest({
     clientId: "390958680596-vtonuian8j15vrjq0sr9k352ddulcs85.apps.googleusercontent.com",
     iosClientId: "390958680596-t0gg07uga0sc89e9h5kdgocuir9ot1uv.apps.googleusercontent.com",
     androidClientId: "390958680596-cspmtm3k11gvk8qtv5581rkee8b2dhh9.apps.googleusercontent.com"
@@ -24,25 +25,24 @@ export default function App() {
       setAccessToken(response.authentication.accessToken);
       accessToken && fetchUserInfo();
     }
-  },[])
+  },[response, accessToken])
 
-  async function fetchUserInfo() {
-    let response = await fetch("https://www.googleapis.com/userinfo/v2/me",{
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    });
-    const useInfo = await response.json();
-    setUser(useInfo);
-  }
+  function fetchUserInfo() {
+    const url="https://www.googleapis.com/userinfo/v2/me";
+    axios.get(url, {headers: {Authorization: `Bearer ${accessToken}`}})
+          .then((res) => setUser(res.data))          
+          .catch((err)=> console.log(err));
+  };
+  console.log(user);
 
-  const ShowUserInfo = ()=>{
+  const ShowUserInfo = ({user})=>{
+    console.log(user);
     if(user) {
       return(
         <View style={{flex: 1, alignItems: 'center',justifyContent: 'center'}}>
-          <Text style={{fontSize: 35, fontWeight: 'bold', marginBottom: 20}}>Welcome</Text>
-          <Image source={{uri: user.picture}} style={{width:100, height:100, borderRadius: 50}} />
-          <Text style={{fontSize: 20, fontWeight: 'bold'}}>{user.name}</Text>
+          <Text style={{fontSize: 35, fontWeight: 'bold', marginBottom: 20}}>You are logged!</Text>
+          <Image source={{uri: user?.picture}} style={{width:100, height:100, borderRadius: 50}} />
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>{user?.name}</Text>
         </View>
       )
     }
@@ -50,20 +50,15 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {user && <ShowUserInfo />}
+      {user && <ShowUserInfo user = {user} />}
       {user === null && 
-         <>
-         <Text style={{fontSize: 35, fontWeight: 'bold'}}>Welcome</Text>
-         <Text style={{fontSize: 25, fontWeight: 'bold', marginBottom: 20, color: 'gray'}}>Please login</Text>
-       <TouchableOpacity
-         disabled={!request}
-         onPress={() => {
-           promptAsync();
-           }} 
-       >
+      <>
+        <Text style={{fontSize: 35, fontWeight: 'bold'}}>Welcome</Text>
+        <Text style={{fontSize: 25, fontWeight: 'bold', marginBottom: 20, color: 'gray'}}>Please login</Text>
+        <TouchableOpacity disabled={!request} onPress={() => promptAsync()} >
          <Image source={require("./btn.png")} style={{width: 300, height: 40}} />
-       </TouchableOpacity>
-       </>
+        </TouchableOpacity>
+      </>
       }
       <StatusBar style="auto" />
     </View>
@@ -78,3 +73,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// async function fetchUserInfo() {
+  //   let res = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+  //     headers: { Authorization: `Bearer ${accessToken}` }
+  //   });
+  //   const useInfo = await res.json();
+  //   setUser(useInfo);
+  //   console.log(useInfo);
+  // }
